da9d8275f7913a466c2bb2be1b957562fd97062a
zhengningning <1514383518@qq.com>
2024-06-27 01:38:05 +0800

    feat: init modify

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ningcn.net": dockerHub,
+  "quay.ningcn.net": "https://quay.io",
+  "gcr.ningcn.net": "https://gcr.io",
+  "k8s-gcr.ningcn.net": "https://k8s.gcr.io",
+  "k8s.ningcn.net": "https://registry.k8s.io",
+  "ghcr.ningcn.net": "https://ghcr.io",
+  "cloudsmith.ningcn.net": "https://docker.cloudsmith.io",
+  "ecr.ningcn.net": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ningcn.net": dockerHub,
 };
 
 function routeByHosts(host) {
